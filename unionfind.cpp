4c5a058e50c316047971bdca0635a156bc702734
chocobo <e1b17065@st.oit.ac.jp>
2018-05-31 21:13:40 +0900

    update

--- unionfind.cpp
+++ unionfind.cpp
@@ -7,11 +7,13 @@ class unionfind
 private:
     vector<long long> par;
     vector<long long> rank;
+    vector<long long> counter;
 public:
     unionfind(long long n);
     long long find(long long x);
     void unite(long long x, long long y);
     bool same(long long x, long long y);
+    long long count(long long x);
 };
 
 // 初期化
@@ -19,6 +21,7 @@ unionfind::unionfind(long long n){
     for (long long i = 0; i < n; i++) {
         par.push_back(i);
         rank.push_back(0);
+        counter.push_back(1);
     }
 }
 
@@ -39,9 +42,13 @@ void unionfind::unite(long long x, long long y) {
     if (x == y)return;
     
     if (rank[x] < rank[y]) {
+        counter[y] += counter[x];
+        counter[x] = counter[y];
         par[x] = y;
     }
     else {
+        counter[y] += counter[x];
+        counter[x] = counter[y];
         par[y] = x;
         if (rank[x] == rank[y]) {
             rank[x]++;
@@ -52,4 +59,9 @@ void unionfind::unite(long long x, long long y) {
 // xとyが同じ集合に属していたらtrue
 bool unionfind::same(long long x, long long y) {
     return find(x) == find(y);
+}
+
+// xと同じ集合に属している要素数を返す
+long long unionfind::count(long long x){
+    return counter[find(x)];
 }
