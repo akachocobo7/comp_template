574ad6bce2f91ee52aad31bda6034e0d98e6be0b
chocobo <e1b17065@st.oit.ac.jp>
2020-04-09 15:44:58 +0900

    update

--- bellmanford.cpp
+++ bellmanford.cpp
@@ -13,7 +13,7 @@ private:
     vector<T> dist;
     
 public:
-    Bellmanford(int n) : n(n), g(n){}
+    Bellmanford(int n) : n(n), g(n), dist(n, INF){}
     
     void add_edge(int v1, int v2, T cost){
         g[v1].push_back({ v2, cost });
@@ -21,15 +21,14 @@ public:
     
     // 負の閉路があるときfalseを返す
     bool run(int s){
-        dist = vector<T>(n, INF);
         dist[s] = 0;    //開始点の距離は0
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < 2 * n; i++) {
             for (int v = 0; v < n; v++) {
                 for (auto& e : g[v]) {
                     if (dist[v] != INF && dist[e.to] > dist[v] + e.cost) {
                         dist[e.to] = dist[v] + e.cost;
-                        if (i == n - 1) {
+                        if (i >= n - 1) {
                             // return false;
                             dist[e.to] = -INF;
                         }
